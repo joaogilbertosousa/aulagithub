8fa5ae4fab5621e52f40ff09af1a30e88847fcc1
Joao Gilberto <joaogilbertosousa@yahoo.com.br>
2026-04-23 22:59:36 -0300

    Criado projeto para curso

---
+++ src/Program.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+void main() {
+
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
+
+	IO.println("Enter three numbers: ");
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int c = sc.nextInt();
+	
+	int higher = max(a, b, c);
+	
+	showResult(higher);
+
+	sc.close();
+	
+
+
+}
+
+
+public static int max(int x, int y, int z) {
+	int aux;
+	
+	if(x > y && x > z) {
+		aux = x;
+	}
+	else if (y > z) {
+		aux = y;
+	}
+	else aux = z;
+	
+	return aux;
+	
+}
+
+public static void showResult(int value) {
+	IO.println("Higher = " + value);
+}
+
+
